eaf50d16a5699bcf83540145971b9baedc705ac6
JAVIERCLO <cha23132@uvg.edu.gt>
2024-08-22 10:59:53 -0600

    Seccion 2 del ejercicio 3
    Creacion de funcion recursiva para calcular la serie de Fibonacci de un numero y el tiempo de ejecucion

--- Lab5_ejercicio3.c
+++ Lab5_ejercicio3.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
 #include <omp.h>
 
+    //Funcion para calcular la serie de Fibonacci
+    int fibonacci (int N){
+        if (N == 0){
+            return 0;
+        }
+        else if (N == 1){
+            return 1;
+        }
+        else{
+            return fibonacci(N - 1) + fibonacci(N - 2);
+        }
+    }
     //Funcion principal
     int main(){
 
@@ -8,8 +20,8 @@
     #pragma omp sections
     {
     //Seccion 1: Calcular el factorial de un numero
-    #pragma omp section
-        {
+        #pragma omp section
+            {
         //Numero a calcular
         int N;
         //Es igual a 1 porque 0! = 1
@@ -23,16 +35,27 @@
         for(int i = 1; i <= N; i++){
                 factorial *=i;
             }
-        // Termina la medición del tiempo
+        // Termina la medicion del tiempo
         double tiempo_fin = omp_get_wtime();
         //Imprime el factorial y el tiempo de ejecucion
         printf("El factorial de %d es %d\n", N, factorial);
+        printf("Tiempo de ejecucion: %f segundos\n", tiempo_fin - tiempo_inicio);
+            }
+        //Seccion 2: Calcular una serie de Fibonacci
+        #pragma omp section
+            {
+        //Numero a calcular
+        int N;
+        //Ingreso del numero
+        printf("Ingresa un numero para calcular su serie de Fibonacci: ");
+        scanf("%d",&N);
+        //Inicia la medicion del tiempo
+        double tiempo_inicio = omp_get_wtime();
+        //Termina la medicion del tiempo
+        double tiempo_fin = omp_get_wtime();
+        //Imprime la serie de Fibonacci y el tiempo de ejecucion
+        printf("La serie de Fibonacci de de %d es %d\n", N, fibonacci);
         printf("Tiempo de ejecucion: %f segundos", tiempo_fin - tiempo_inicio);
-        }
-    //Seccion 2
-    #pragma omp section
-        {
-        
+            }
         }
     }
-}
