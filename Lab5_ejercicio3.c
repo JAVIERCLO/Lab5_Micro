0d2fd2dc434551cdb92a3a490b083baf86b648a6
JAVIERCLO <cha23132@uvg.edu.gt>
2024-08-22 12:14:49 -0600

    Seccion 3 del ejercicio 3
    Creacion de la 3 seccion y correccion de errores de las primeras 2 secciones

--- Lab5_ejercicio3.c
+++ Lab5_ejercicio3.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <omp.h>
 
     //Funcion para calcular la serie de Fibonacci
@@ -22,40 +23,64 @@
     //Seccion 1: Calcular el factorial de un numero
         #pragma omp section
             {
-        //Numero a calcular
-        int N;
-        //Es igual a 1 porque 0! = 1
-        long long factorial = 1;
-        //Ingreso del numero
-        printf("Ingresa un numero para calcular su factorial: ");
-        scanf("%d",&N);
-        //Inicia la medicion del tiempo
-        double tiempo_inicio = omp_get_wtime();
-        //Ciclo for para calcular el factorial
-        for(int i = 1; i <= N; i++){
-                factorial *=i;
-            }
-        // Termina la medicion del tiempo
-        double tiempo_fin = omp_get_wtime();
-        //Imprime el factorial y el tiempo de ejecucion
-        printf("El factorial de %d es %d\n", N, factorial);
-        printf("Tiempo de ejecucion: %f segundos\n", tiempo_fin - tiempo_inicio);
+                //Numero a calcular
+                int N = 100000;
+                //Es igual a 1 porque 0! = 1
+                unsigned long long factorial = 1;
+                //Inicia la medicion del tiempo
+                double tiempo_inicio = omp_get_wtime();
+                //Ciclo for para calcular el factorial
+                for(int i = 1; i <= N; i++){
+                        factorial *=i;
+                    }
+                // Termina la medicion del tiempo
+                double tiempo_fin = omp_get_wtime();
+                //Imprime el factorial y el tiempo de ejecucion
+                printf("El factorial de %d es %d\n", N, factorial);
+                printf("Tiempo de ejecucion: %f segundos\n", tiempo_fin - tiempo_inicio);
             }
         //Seccion 2: Calcular una serie de Fibonacci
         #pragma omp section
             {
-        //Numero a calcular
-        int N;
-        //Ingreso del numero
-        printf("Ingresa un numero para calcular su serie de Fibonacci: ");
-        scanf("%d",&N);
-        //Inicia la medicion del tiempo
-        double tiempo_inicio = omp_get_wtime();
-        //Termina la medicion del tiempo
-        double tiempo_fin = omp_get_wtime();
-        //Imprime la serie de Fibonacci y el tiempo de ejecucion
-        printf("La serie de Fibonacci de de %d es %d\n", N, fibonacci);
-        printf("Tiempo de ejecucion: %f segundos", tiempo_fin - tiempo_inicio);
+                //Numero a calcular
+                int N = 1000000;
+                //Inicia la medicion del tiempo
+                double tiempo_inicio = omp_get_wtime();
+                //Termina la medicion del tiempo
+                
+                double tiempo_fin = omp_get_wtime();
+                //Imprime la serie de Fibonacci y el tiempo de ejecucion
+                printf("La serie de Fibonacci de de %d es %d\n", N, fibonacci);
+                printf("Tiempo de ejecucion: %f segundos\n", tiempo_fin - tiempo_inicio);
+            }
+        // Seccion 3: Encontrar el maximo de un arreglo
+        #pragma omp section
+            {
+                // Longitud del arreglo
+                int L = 1000000;
+                printf("Calculando el maximo de un arreglo de %d elementos:\n", L);
+                // Inicia la medicion del tiempo
+                double tiempo_inicio = omp_get_wtime();
+                // Arreglo utilizando malloc
+                int *array = (int *)malloc(L * sizeof(int));
+                //Llenar el arreglo con valores aleatorios
+                for (size_t i = 0; i < L; i++){
+                    array[i] = rand();
+                }
+                // Asignar un valor maximo inicial
+                int max = array[0];
+                // Ciclo para comparar los valores del arreglo hasta encontrar el maximo
+                for (int i = 1; i < L; i++){
+                    if (array[i] > max){
+                        max = array[i];
+                    }
+                }
+                // Termina la medicion del tiempo
+                double tiempo_fin = omp_get_wtime();
+                // Imprime el maximo del arreglo y el tiempo de ejecucion
+                printf("El maximo del arreglo es: %d y el tiempo de ejecucion es: %f segundos\n", max, tiempo_fin - tiempo_inicio);
+                // Liberar memoria
+                free(array);
             }
         }
     }
