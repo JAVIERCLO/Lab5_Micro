01c117adf3031777528ddc0b1f5bc39c44d90dfd
JAVIERCLO <cha23132@uvg.edu.gt>
2024-08-23 10:36:15 -0600

    Creacion del ejercicio 4
    Falta probar, solo está el codigo escrito

---
+++ Lab5_ejercicio4.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <omp.h>
+//Funcion principal
+int main(){
+    //Creacion de variable compartida y privada
+    int N = 10;
+    int varComp = 0;
+    int varPriv = 0;
+    //Inicia la medicion del tiempo
+    double tiempo_inicio = omp_get_wtime();
+
+    #pragma omp parallel for shared(varComp) private(varPriv)
+    //Ciclo paralelo con una variable compartida y una privada
+    for(int i = 0; i < N; i++ ){
+        varComp += 1;
+        varPriv = 1;
+    }
+    //Termina la medicion del tiempo
+    double tiempo_fin = omp_get_wtime();
+    //Impresion de resultados y tiempo de ejecucion
+    printf("Hilo numero %d, la variable compartida es %d, y la variable privada es %d, y el tiempo de ejecucion es %d\n",omp_get_thread_num(),varComp,varPriv,tiempo_fin-tiempo_inicio);
+}
